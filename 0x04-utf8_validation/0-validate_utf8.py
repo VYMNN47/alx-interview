ad720c2b4c3369530181804ced50a8a9ab0e52de
aymnn <ayman.belahsen.4@gmail.com>
2024-08-22 01:51:36 +0100

    0101

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module for validUTF8 function """
+
+
+def validUTF8(data):
+    """determines if a given data set represents a valid UTF-8 encoding"""
+    num_bytes = 0
+
+    for byte in data:
+        byte = byte & 0xFF
+        if num_bytes == 0:
+            if (byte >> 5) == 0b110:
+                num_bytes = 1
+            elif (byte >> 4) == 0b1110:
+                num_bytes = 2
+            elif (byte >> 3) == 0b11110:
+                num_bytes = 3
+            elif (byte >> 7) == 0:
+                num_bytes = 0
+            else:
+                return False
+        else:
+            if (byte >> 6) != 0b10:
+                return False
+            num_bytes -= 1
+
+    return num_bytes == 0
