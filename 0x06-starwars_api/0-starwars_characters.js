46e931011d282071dd42961bcdfe7e11fe4dd34c
aymnn <ayman.belahsen.4@gmail.com>
2024-09-03 16:39:29 +0100

    .

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,62 +1,17 @@
 #!/usr/bin/node
+
 const request = require('request');
 
-const filmNumber = process.argv[2];
-
-if (!filmNumber) {
-  console.error('Please provide a film number.');
-  process.exit(1);
-}
-
-function fetchCharacterDetails (url) {
-  return new Promise((resolve, reject) => {
-    request(url, (error, response, body) => {
-      if (error) {
-        return reject(error);
-      }
-      if (response.statusCode === 200) {
-        try {
-          const data = JSON.parse(body);
-          resolve(data.name);
-        } catch (e) {
-          reject(e);
-        }
-      } else {
-        reject(new Error('Response Status: ' + response.statusCode));
-      }
-    });
-  });
-}
-
-function fetchCharacters (data) {
-  const characterList = data.characters;
-  const characterPromises = characterList.map(fetchCharacterDetails);
-
-  Promise.all(characterPromises)
-    .then(characterNamesList => {
-      characterNamesList.forEach(name => {
-        console.log(name);
-      });
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-}
-
-const url = `https://swapi-api.alx-tools.com/api/films/${filmNumber}`;
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error: ', error);
-    return;
-  }
-  if (response.statusCode === 200) {
-    try {
-      const data = JSON.parse(body);
-      fetchCharacters(data);
-    } catch (e) {
-      console.error('Error parsing film JSON:', e);
-    }
-  } else {
-    console.log('Response Status:', response.statusCode);
-  }
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
 });
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
